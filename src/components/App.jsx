22ee0ca29bb07dfba0bebf9992a186707fc5dab3
Veronika <nikakras.dance@gmail.com>
2023-09-15 22:20:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 
 import { Profile } from "components/Profile/Profile"
-import { Statistics} from "components/Statistics/Statistics"
+// import { Statistics} from "components/Statistics/Statistics"
 import user from "../data/user.json"
-import data from "../data/data.json"
+// import data from "../data/data.json"
 
 export const App = () => {
   return ( 
