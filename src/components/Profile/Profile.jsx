28a1fb1fe5eb77eb152f3ac4d999d287fe08cc40
Veronika <nikakras.dance@gmail.com>
2023-09-16 12:11:10 +0300

    1 done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -17,15 +17,15 @@ export const Profile = ({username, tag, location, avatar,  stats:{followers,view
 
         <ul className={ css.stats}>
     <li>
-            <span className={ css.label}>Followers</span>
+            <span className={ css.label}>Followers </span>
             <span className={ css.quantity}>{ followers}</span>
     </li>
     <li>
-            <span className={ css.label}>Views</span>
+            <span className={ css.label}>Views </span>
             <span className={ css.quantity}>{ views}</span>
     </li>
     <li>
-            <span className={ css.label}>Likes</span>
+            <span className={ css.label}>Likes </span>
             <span className={ css.quantity}>{likes}</span>
     </li>
   </ul>
